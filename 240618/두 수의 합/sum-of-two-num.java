785c431ec547eefddca984728b2b8823fe0ed487
heeeeee0129 <86648265+heeeeee0129@users.noreply.github.com>
2024-06-18 12:56:16 +0900

    Title: 두 수의 합, Time: 414ms, Memory: 26MB, Status: Passed - CodeTree

--- 240618/두 수의 합/sum-of-two-num.java
+++ 240618/두 수의 합/sum-of-two-num.java
@@ -3,33 +3,34 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
-
+// (target_sum - 값)이 HashMap에 존재하는 지 확인
+// −10^9 ≤ 정수 ≤ 10^9 -> long
+// 
 public class Main {
     public static int n;
-    public static int k;
-    public static HashMap<Integer, Integer> map = new HashMap<>();
-    public static int count = 0;
+    public static long k;
+    public static long[] arr;
+    public static HashMap<Long, Integer> count = new HashMap<>();
+    public static int result = 0;
 
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(in.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
+        arr = new long[n];
 
         st = new StringTokenizer(in.readLine());
         for(int i = 0; i<n; i++){
-            map.put(i, Integer.parseInt(st.nextToken()));
+            arr[i] = Long.parseLong(st.nextToken());
         }
 
         for(int i = 0; i<n; i++){
-            for(int j = i+1; j<n; j++){
-                int sum = map.get(i)+ map.get(j);
-                if(sum == k){
-                    count++;
-                }
-            }
+            long target = k - arr[i];
+            result += count.getOrDefault(target, 0);
+            count.put(arr[i], count.getOrDefault(arr[i], 0)+1);
         }
+        System.out.print(result);
         
-        System.out.print(count);
     }
 }
