77450db7c08cdc0d809e2c0f395ad319c7a5e91d
heeeeee0129 <86648265+heeeeee0129@users.noreply.github.com>
2024-06-19 18:17:39 +0900

    Title: 세 수의 합, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240619/세 수의 합/sum-of-three-num.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static int n;
+    public static long k;
+    public static long[] arr;
+    public static HashMap<Long, Integer> count = new HashMap<>();
+    public static int result = 0;
+
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Long.parseLong(st.nextToken());
+        arr = new long[n];
+        st = new StringTokenizer(in.readLine());
+        for(int i = 0; i< n; i++){
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+        for(int i = 0; i<n-1; i++){
+            for(int j = i+1; j<n; j++){
+                long target = k - arr[i] - arr[j];
+                result += count.getOrDefault(target, 0);
+                count.put(arr[i]+arr[j], count.getOrDefault(arr[i]+arr[j], 0)+1);
+            }
+        }
+        System.out.println(result);
+    }
+}
