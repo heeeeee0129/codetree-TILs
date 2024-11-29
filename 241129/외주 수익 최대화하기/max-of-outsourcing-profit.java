c203ed8de76d986f02e91dfee5aee604d0c7879a
heeeeee0129 <86648265+heeeeee0129@users.noreply.github.com>
2024-11-29 15:12:48 +0900

    Title: 외주 수익 최대화하기, Time: 90ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241129/외주 수익 최대화하기/max-of-outsourcing-profit.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] times;
+    static int[] pays;
+    static int[] dp;
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(in.readLine());
+        times = new int[n];
+        pays = new int[n];
+        dp = new int[n+1];
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(in.readLine());
+            times[i] = Integer.parseInt(st.nextToken());
+            pays[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i<n; i++){
+            int d = dp[i+times[i]];
+            int new_d = dp[i] + pays[i];
+            if (d < new_d){
+                dp[i+times[i]] = new_d;
+            }
+        }
+        System.out.print(dp[n]);
+        // for(int i = 0; i<=n; i++){
+        //     System.out.print(dp[i]+ " " );
+        // }
+
+    }
+}
