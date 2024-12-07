351d55c9731d626dc301e1da0cbf3fa3ab4758ad
heeeeee0129 <86648265+heeeeee0129@users.noreply.github.com>
2024-12-07 14:03:32 +0900

    Title: 수 선택 순서, Time: 227ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241207/수 선택 순서/number-selection-order.java
+++ 241207/수 선택 순서/number-selection-order.java
@@ -1,36 +1,46 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(in.readLine());
-        Queue<Integer> queue = new ArrayDeque<>();
-        PriorityQueue<Integer> pqueue = new PriorityQueue<>();
-
         int n = Integer.parseInt(st.nextToken());
         int x = Integer.parseInt(st.nextToken());
-
+        Queue<Node> queue = new ArrayDeque<>();
+        PriorityQueue<Integer> pqueue = new PriorityQueue<>((e1, e2) -> Integer.compare(e2, e1));
         st = new StringTokenizer(in.readLine());
+        
         for(int i = 0; i<n; i++){
             int temp = Integer.parseInt(st.nextToken());
-            queue.add(temp);
-            pqueue.add(-temp);
+            pqueue.add(temp);
+            queue.add(new Node(temp, i));
         }
         int count = 1;
         while(true){
-            int max_queue = -pqueue.peek();
-            int queue_number = queue.poll();
-            if(queue_number == x){
-                System.out.print(count);
+            int max_number = pqueue.peek();
+            Node head = queue.poll();
+            if(head.index == x){
+                System.out.println(count);
                 break;
             }
-            if(queue_number != max_queue){
-                queue.add(queue_number);
+            if(max_number == head.number){
+                pqueue.poll();
+                count++;
+            }else{
+                queue.add(head);
             }
-            count++;
         }
-        
+
+    }
+
+    public static class Node {
+        int number;
+        int index;
+
+        public Node(int number, int index){
+            this.number = number;
+            this.index = index;
+        }
     }
 }
