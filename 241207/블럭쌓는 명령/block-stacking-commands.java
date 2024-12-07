7dfeb346cb785d14ca3de8a30757346b93dd7804
heeeeee0129 <86648265+heeeeee0129@users.noreply.github.com>
2024-12-07 13:21:35 +0900

    Title: 블럭쌓는 명령, Time: 89ms, Memory: 9MB, Status: Runtime Error - CodeTree

--- 241207/블럭쌓는 명령/block-stacking-commands.java
+++ 241207/블럭쌓는 명령/block-stacking-commands.java
@@ -6,19 +6,25 @@ public class Main {
         StringTokenizer st = new StringTokenizer(in.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[] count_arr = new int[n];
+        int[] d_arr = new int[n];
         for(int i = 0; i<k; i++){
             st = new StringTokenizer(in.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            for(int j = a-1; j<b; j++){
-                count_arr[j]++;
-            }
+            int a = Integer.parseInt(st.nextToken())-1;
+            int b = Integer.parseInt(st.nextToken())-1;
+            d_arr[a]++;
+            d_arr[b+1]--;
         }
-        Arrays.sort(count_arr);
-        System.out.print(count_arr[n/2]);
-        // for(int num: count_arr){
-        //     System.out.print(num + " " );
-        // }
+
+        int[] num_arr = new int[n];
+        int count = 0;
+        
+        for(int i = 0; i<n; i++){
+            count += d_arr[i];
+            num_arr[i] = count;
+        }
+
+        Arrays.sort(num_arr);
+        System.out.print(num_arr[n/2]);
+        
     }
 }
